01230d8d1c032129331ba1d25426b2f2e40ec482
MakerSquare Student <students@themakersquare.com>
2013-11-23 17:00:55 -0600

    added private methods

--- bowling.rb
+++ bowling.rb
@@ -14,12 +14,19 @@ class BowlingGame
 		i, last, second_last, third_last = 0, 0, 0, 0
 		@rolls.inject(0) do |mem, current|
 			i += 1
-			mem += current if (last + second_last) > 9 && i.odd? && (second_last != 10 || i == 21)
-			mem += current + last if third_last == 10 
+			mem += current if  spare?(last, second_last, i)
+			mem += current + last if strike?(third_last) 
 			mem += current unless i == 21
 			last, second_last, third_last = current, last, second_last
 			mem
 		end
 	end
-	
+
+	private
+	def spare?(last, second_last, i)
+		(last + second_last) > 9 && i.odd? && (second_last != 10 || i == 21)
+	end
+	def strike?(third_last)
+		third_last == 10
+	end
 end
